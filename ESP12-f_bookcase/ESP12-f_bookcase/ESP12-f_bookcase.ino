8a2d75cf687b61197e0a4a3e812f581932a12251
TNTinMinecraft <hedusong0616@163.com>
2023-03-26 22:49:32 +0800

    Update ESP12-f_bookcase.ino
    Change mqtt from THUB to Json

--- ESP12-f_bookcase/ESP12-f_bookcase/ESP12-f_bookcase.ino
+++ ESP12-f_bookcase/ESP12-f_bookcase/ESP12-f_bookcase.ino
@@ -8,10 +8,13 @@
   by TNTInMinecraft
   modified 20 Jan 2023
   by TNTInMinecraft
+  Change mqtt from THUB to Json 26 Mar 2023
+  by TNTInMinecraft
 */
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 // set device name and place
 String DeviceName = "bookcase";
@@ -25,6 +28,7 @@ int DataPin = 14;   // Data pin (DS)
 // WiFi
 const char *ssid = "***";         // WiFi name
 const char *password = "***";  // WiFi password
+String MAC = "";
 
 // MQTT Broker
 const char *mqtt_broker = "192.168.31.153";
@@ -34,10 +38,13 @@ const char *mqtt_password = "";
 const int mqtt_port = 1883;
 
 // LED_bit
-int bit_data[16] = { 1 };
+int bit_data[16] = {1};
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+// set json memory
+StaticJsonDocument<200> mqtt_json;
 
 void setup() {  
   // set out pin to 74hc595
@@ -49,13 +56,15 @@ void setup() {
 
   // connecting to a WiFi network
   WiFi.begin(ssid, password);
+  Serial.println("");
   Serial.println("Connecting to WiFi.");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("Connected to the WiFi network.IP addrress: ");
+  Serial.println("Connected to the WiFi network IP addrress: ");
   Serial.println(WiFi.localIP());
+  MAC = WiFi.macAddress();
 
   //set mqtt client name
   String client_id = "THUB." + DeviceName + "." + Place + "." + String(WiFi.macAddress());
@@ -75,69 +84,65 @@ void setup() {
   }
 
   // set client name
-  String UpData = "THUB." + DeviceName + "." + Place + String(WiFi.macAddress()) + ".online";
-  byte pubSiz = UpData.length() + 1;
-  char pubmsg[pubSiz];
-
-  // publish and subscribe
-  UpData.toCharArray(pubmsg, pubSiz);
+  char pubmsg[100];
+  mqtt_json["id"] = MAC;
+  mqtt_json["device"] = DeviceName;
+  mqtt_json["place"] = Place;
+  serializeJson(mqtt_json, pubmsg, sizeof(pubmsg));
   client.publish(topic, pubmsg);
   client.subscribe(topic);
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
   String message;
-  String in_message[8] = { "" };
+
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
   Serial.print("Message:");
   for (int i = 0; i < length; i++) {
     message = message + (char)payload[i];
   }
+
   Serial.println(message);
-  int j = 0;
-  for (int i = 0; i < message.length(); i++) {
-    if (message[i] == '.') {
-      j++;
+
+  // deserialize json
+  DeserializationError error = deserializeJson(mqtt_json, message);
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  // judgement id and modes
+  if (mqtt_json["id"] == MAC) {
+    if (mqtt_json["modes"] == "default") {
+      Serial.println(String(mqtt_json["data"]));
+      modes_control(mqtt_json["data"]);
+    } else if (mqtt_json["modes"] == "bit") {
+      for (int i = 0; i < 16; i++){
+        bit_data[i] = mqtt_json["data"][i];
+      }
     } else {
-      in_message[j] = in_message[j] + char(message[i]);
+      return;
     }
+    light_control();
   }
-  if (in_message[0].compareTo("THUB") && in_message[1].compareTo(DeviceName) && in_message[2].compareTo(Place) && in_message[3].compareTo(WiFi.macAddress())) {
-    if (in_message[4] == "bit") {
-      cut_message(in_message[5]);  // cut message
-      light_control();
-    } else if (in_message[4] == "mode") {
-      light_control();
-    }
-  }
+
   // debug
   debug_LED();
   Serial.println("-----------------------");
 }
 
-void cut_message(String cut_message) {
-  int j = 0;
-  for (int i = 0; i < cut_message.length(); i++) {
-    if (cut_message[i] == ',') {
-      j++;
-    } else {
-      bit_data[j] = bit_data[j] * 10 + (cut_message[i] - '0');
-    }
-  }
-  cut_message = "";
-}
-
-void modes(String in_mode) {
-  if (in_mode.compareTo("all_off")) {
+void modes_control(const char* in_mode) {
+  if (in_mode == "all_off") {
     for (int i = 0; i < 16; i++) {
       bit_data[i] = 0;
     }
-  } else if (in_mode.compareTo("all_on")) {
+  } else if (in_mode == "all_on") {
     for (int i = 0; i < 16; i++) {
       bit_data[i] = 1;
     }
-  } else if (in_mode.compareTo("on_up")) {
+  } else if (in_mode == "on_up") {
     for (int i = 0; i < 16; i++) {
       bit_data[i] = 1;
       i++;
@@ -147,8 +152,8 @@ void modes(String in_mode) {
   }
 }
 
+// output light state
 void light_control() {
-  // output light state
   digitalWrite(BitPin, LOW);
   for (int i = 15; i >= 0; i--) {
     digitalWrite(DataPin, bit_data[i]);
